4c077684acd6f18bbd6a57ef7cccb2fbb8ad7f3f
snare-dev <amanisanare1@gmail.com>
2024-05-23 08:39:36 -0400

    changed address

--- controllers/googleLogin.js
+++ controllers/googleLogin.js
@@ -20,7 +20,7 @@ const auth = passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/google/callback",
+      callbackURL: "http://localhost:8000/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       const existingUser = await User.findOne({
